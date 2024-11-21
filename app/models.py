d58cf916b33a37ceb746637f170bc9d743148f26
2Rahul2 <rahulbclg24@gmail.com>
2024-11-21 22:15:11 +0530

    Ooo

--- app/models.py
+++ app/models.py
@@ -1,12 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 # from django.contrib.auth.models import User
+from django.contrib.auth.models import BaseUserManager
 
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError("The Username field must be set")
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)  # Ensure the password is hashed
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username, password, **extra_fields)
 
 class User(AbstractUser):
     is_faculty = models.BooleanField(default=False)
 
+    objects = CustomUserManager()  
 class Subject(models.Model):
     name = models.CharField(max_length=100)
     # faculties = models.ManyToManyField(Faculty, through='SubjectFaculty')
