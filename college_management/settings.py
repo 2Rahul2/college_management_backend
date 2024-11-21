64b12e451a61c4916157b20c600fbb60d090ae4a
2Rahul2 <rahulbclg24@gmail.com>
2024-11-21 13:56:06 +0530

    rest

--- college_management/settings.py
+++ college_management/settings.py
@@ -45,7 +45,11 @@ INSTALLED_APPS = [
 ]
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.SessionAuthentication',  # Requires CSRF
+        'rest_framework.authentication.BasicAuthentication',    # Optional fallback
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
     ],
     # Add other settings as necessary
 }
