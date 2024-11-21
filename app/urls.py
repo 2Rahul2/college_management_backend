b425216129562157bb7b0f4b157f9d25956aed5c
2Rahul2 <rahulbclg24@gmail.com>
2024-11-21 19:43:40 +0530

    building

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import index , FacultyViewSet,StudentEnrollmentView ,Health , StudentEnrolledSubjectsView ,LoginView ,RowCountView ,ReturnUserid , SubjectWithFaculties ,ReturnRole,LogoutView, StudentViewSet,SubjectViewSet,AssignFacultyToStudentView ,RegisterView , StudentListView , StudentDetailView
+from .views import  FacultyViewSet,StudentEnrollmentView ,Health , StudentEnrolledSubjectsView ,LoginView ,RowCountView ,ReturnUserid , SubjectWithFaculties ,ReturnRole,LogoutView, StudentViewSet,SubjectViewSet,AssignFacultyToStudentView ,RegisterView , StudentListView , StudentDetailView
 
 # router = DefaultRouter()
 # router.register(r'faculties', FacultyViewSet)
@@ -8,10 +8,10 @@ from .views import index , FacultyViewSet,StudentEnrollmentView ,Health , Studen
 # router.register(r'subjects', SubjectViewSet)
 
 urlpatterns = [
-    path('' ,index ,name="index"),
+    # path('' ,index ,name="index"),
     path('health/' , Health.as_view()),
-     path('api/login/', LoginView.as_view(), name='login'),
-    path('api/logout/', LogoutView.as_view(), name='logout'),
+     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     # path('api/', include(router.urls)),
     path('student-count/' , RowCountView.as_view() ,name="student_count"),
     # path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
--- college_management/urls.py
+++ college_management/urls.py
@@ -25,8 +25,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('' ,include(urlpatterns)),
-    # path('react/', TemplateView.as_view(template_name="index.html")),
+    path('api/' ,include(urlpatterns)),
+    path('', TemplateView.as_view(template_name="index.html")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
